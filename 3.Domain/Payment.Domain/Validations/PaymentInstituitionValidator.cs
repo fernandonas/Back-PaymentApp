ba901dcbc4774ec4b07706ef7a928ad4ee300ced
Fernando Augusto do Nascimento <fernando.nascimento@ambevtech.com.br>
2022-09-16 14:35:52 -0300

    Adicionado PaymentInstituitionValidator.

---
+++ 3.Domain/Payment.Domain/Validations/PaymentInstituitionValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Payment.Domain.Entity;
+
+namespace Payment.Domain.Validations
+{
+    public class PaymentInstituitionValidator : AbstractValidator<PaymentInstituition>
+    {
+        public PaymentInstituitionValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Nome não informado.");
+        }
+    }
+}
